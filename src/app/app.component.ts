b1e4058e2e356bcc86c6acd92dc4e40517f5f231
nidhisahu0902 <nidhinids0902@gmail.com>
2021-01-05 22:29:13 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import {Message,MessageService} from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService]
+    
 })
 export class AppComponent {
+
+
+/*88888888888 show messages ***************************************/
+  msgs2: Message[];
   title = 'exampl2ongprime';
+
+  data: any;
+  constructor(private primengConfig: PrimeNGConfig,private messageService: MessageService) {
+
+    /*******************chart*********** */
+    this.data = {
+      labels: ['A','B','C'],
+      datasets: [
+          {
+              data: [300, 50, 100],
+              backgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56"
+              ],
+              hoverBackgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56"
+              ]
+          }]    
+      };
 }
+  
+
+  showSuccess() {
+    this.messageService.add({severity:'success', summary: 'Success', detail: 'Message Content'});
+}
+
+
+ngOnInit() {
+      
+        this.primengConfig.ripple = true;
+    }
+
+    addMessages() {
+      this.msgs2 = [
+          {severity:'success', summary:'Success', detail:'Message Content1'},
+          {severity:'info', summary:'Info', detail:'Message Content1'},
+          {severity:'warn', summary:'Warning', detail:'Message Content1'},
+          {severity:'error', summary:'Error', detail:'Message Content1'}
+      ];
+  }
+
+  clearMessages() {
+      this.msgs2 = [];
+  }
+
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {ChartModule} from 'primeng/chart';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    ToastModule,
+    MessagesModule,
+    MessageModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
